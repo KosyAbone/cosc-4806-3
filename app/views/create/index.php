b0b004684d2e6cf8e0e08399d5179eae09a9c1fb
Kosiso Abone <abonekosiso@gmail.com>
2025-06-17 02:08:48 +0000

    Copied my create view html from assignment 2 code into this newly created view 'create' and linked accordingly between login and create pages

---
+++ app/views/create/index.php
@@ -0,0 +1,35 @@
+<?php require_once 'app/views/templates/headerPublic.php'?>
+
+<main style="text-align: center">
+    <h2>Create Account</h2>
+
+    <form method="POST" action="validate.php">
+        <input type="hidden" name="action" value="signup">
+
+        <label>Select Username:</label><br>
+        <input type="text" name="username" required><br><br>
+
+        <label>Choose a Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <label>Confirm Password:</label><br>
+        <input type="password" name="confirm_password" required><br>
+        <small style="color: blue;">
+            Password must be at least 8 characters long and include at least one uppercase and one lowercase letter.
+        </small><br>
+
+        <?php
+            if (isset($_SESSION['auth_msg'])) {
+                $msg = $_SESSION['auth_msg'];
+                unset($_SESSION['auth_msg']);
+                echo "<p><strong>" . $msg . "</strong></p>";
+            }
+        ?>
+
+        <input type="submit" value="Register">
+    </form>
+
+    <p>Already have an account?<a href="login"> Login here</a></p>
+
+    <p><a href="index.php">Back to Home</a></p>
+</main>
